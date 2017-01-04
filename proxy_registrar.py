3d14db47575af2d34a7774c5cedbef10bc004026
luisfernandezjimenez <luisfernandez_1993@hotmail.es>
2017-01-04 11:59:10 +0100

    Parte basica terminada y algunas avanzadas realizadas con pep8 y pep257

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -38,6 +38,7 @@ class ProxyHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         u"""Implementación de los Metodos INVITE, ACK y BYE."""
         # Escribe dirección y puerto del cliente (de tupla client_address)
+        self.json2registered()
         IP_CLIENTE = str(self.client_address[0])
         PUERTO_CLIENTE = self.client_address[1]
         while 1:
